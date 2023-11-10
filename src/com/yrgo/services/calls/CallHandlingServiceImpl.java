fe0417a69495c826d5a278506d39818ab9b6c166
Roed doed <hugraj0216@skola.goteborg.se>
2023-11-10 17:08:10 +0100

    wrote some code

--- src/com/yrgo/services/calls/CallHandlingServiceImpl.java
+++ src/com/yrgo/services/calls/CallHandlingServiceImpl.java
@@ -1,2 +1,23 @@
-package com.yrgo.services.calls;public class CallHandlingServiceImpl {
+package com.yrgo.services.calls;
+
+import com.yrgo.domain.Action;
+import com.yrgo.domain.Call;
+import com.yrgo.services.customers.CustomerManagementService;
+import com.yrgo.services.customers.CustomerNotFoundException;
+import com.yrgo.services.diary.DiaryManagementService;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.Collection;
+
+public class CallHandlingServiceImpl implements CallHandlingService{
+    @Override
+    public void recordCall(String customerId, Call newCall, Collection<Action> actions) throws CustomerNotFoundException {
+        ClassPathXmlApplicationContext cointainer = new ClassPathXmlApplicationContext("application.xml");
+        CustomerManagementService customerManagementService = cointainer.getBean(CustomerManagementService.class);
+        DiaryManagementService diaryManagementService = cointainer.getBean(DiaryManagementService.class);
+
+         customerManagementService.recordCall(customerId, newCall);
+
+         actions.forEach(diaryManagementService::recordAction);
+    }
 }
