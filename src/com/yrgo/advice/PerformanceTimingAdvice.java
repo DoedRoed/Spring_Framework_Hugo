846791a8ec186f8fde8b1bb9a744d6ad5fffb4f2
Roed doed <hugraj0216@skola.goteborg.se>
2023-12-08 10:10:56 +0100

    add skeleton and some xml

---
+++ src/com/yrgo/advice/PerformanceTimingAdvice.java
@@ -0,0 +1,33 @@
+package com.yrgo.advice;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+
+public class PerformanceTimingAdvice {
+
+    public Object performTimingMeasurement (ProceedingJoinPoint method) throws Throwable {
+
+        //before
+        long startTime = System.nanoTime();
+
+        try {
+            //proceed to target
+            Object value = method.proceed();
+
+
+            return value;
+        } finally {
+            //after
+            long endTime = System.nanoTime();
+            long timeTaken = endTime - startTime;
+            System.out.println("The method " + method.getSignature().getName() + " took " + timeTaken/1000000);
+        }
+    }
+
+    //before advice
+    public void beforeAdviceTesting(JoinPoint jp) {
+        System.out.println("Now entering method " +
+                jp.getSignature().getName());
+    }
+
+}
